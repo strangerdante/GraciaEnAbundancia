c1ddb325809cc2ac2f2a7b6eca09514583645898
Jamel <strangerdante@gmail.com>
2024-07-24 09:18:44 -0500

    arreglo general

--- src/components/Eventos/ProximosEventos.vue
+++ src/components/Eventos/ProximosEventos.vue
@@ -365,7 +365,7 @@ espacio organizador -->
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-7 w-7"
+            class="h-8 w-8"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
