f06d43ca801f3705ae5c30037b648cae7ccb84fb
Jamel <strangerdante@gmail.com>
2024-08-12 09:32:04 -0500

    icono agregado a imagenes

--- src/components/Eventos/EventoModal.vue
+++ src/components/Eventos/EventoModal.vue
@@ -27,7 +27,7 @@
       <div class="flex flex-col md:flex-row">
         <div
           v-if="evento.banner !== null"
-          class="md:w-1/2 mb-4 md:mb-0 md:mr-4 flex flex-col items-center"
+          class="md:w-1/2 mb-4 md:mb-0 md:mr-4 flex flex-col items-center relative"
         >
           <img
             :src="evento.banner"
@@ -35,41 +35,23 @@
             class="w-2/3 h-auto rounded-lg cursor-pointer"
             @click="ampliarImagen"
           />
-          <div class="flex mt-2">
-            <button
-              @click="ampliarImagen"
-              class="mr-2 p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300"
-              title="Ampliar imagen"
+          <div
+            class="absolute top-2 right-2 bg-white bg-opacity-75 rounded-full p-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 text-gray-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-            <button
-              @click="guardarImagen"
-              class="p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition duration-300"
-              title="Guardar imagen"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z"
-                />
-              </svg>
-            </button>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"
+              />
+            </svg>
           </div>
         </div>
         <div
@@ -152,30 +134,6 @@ export default {
         event.target.tagName === "BUTTON"
       ) {
         this.imagenAmpliada = false;
-      }
-    },
-    async guardarImagen() {
-      try {
-        // Fetch the image
-        const response = await fetch(this.evento.banner);
-        const blob = await response.blob();
-
-        // Create a blob URL
-        const blobUrl = URL.createObjectURL(blob);
-
-        // Create a link element and trigger the download
-        const link = document.createElement("a");
-        link.href = blobUrl;
-        link.download = `${this.evento.titulo}.jpg`; // You can change the extension if needed
-        document.body.appendChild(link);
-        link.click();
-
-        // Clean up
-        document.body.removeChild(link);
-        URL.revokeObjectURL(blobUrl);
-      } catch (error) {
-        console.error("Error al descargar la imagen:", error);
-        // You might want to show an error message to the user here
       }
     },
   },
